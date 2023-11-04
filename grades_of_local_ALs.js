9c1116415a3280c8abfeddc259af92e61721a419
Lakshan-Wijesekara <lwijesekara149@gmail.com>
2023-11-04 20:48:22 +0530

    added the JS file

---
+++ grades_of_local_ALs.js
@@ -0,0 +1,13 @@
+//An application to indicate grades of local ALs
+score=65;
+if (score>=75){
+    console.log('A-Distinction Pass');
+}else if (score>=65){
+    console.log('B-Very Good Pass');
+}else if (score>=55){
+    console.log('C-Credit Pass');
+}else if (score>=40){
+    console.log('D-Ordinary Pass');
+}else if (score>=0){
+    console.log('F-Fail');
+}
