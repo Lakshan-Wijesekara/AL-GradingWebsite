a071b95f2f79839ba5c7c02c7683914023175cda
Lakshan-Wijesekara <lwijesekara149@gmail.com>
2023-11-18 09:26:09 +0530

    Finished stage

--- grades_of_local_ALs.js
+++ grades_of_local_ALs.js
@@ -1,13 +1,20 @@
 //An application to indicate grades of local ALs
-score=65;
-if (score>=75){
-    console.log('A-Distinction Pass');
-}else if (score>=65){
-    console.log('B-Very Good Pass');
-}else if (score>=55){
-    console.log('C-Credit Pass');
-}else if (score>=40){
-    console.log('D-Ordinary Pass');
-}else if (score>=0){
-    console.log('F-Fail');
+
+function checkGrades() {
+    var score = document.getElementById('grade').value;
+    var result= '';
+    if (score>=75){
+        result='A-Distinction Pass';
+    }else if (score>=65){
+        result='B-Very Good Pass';
+    }else if (score>=55){
+        result='C-Credit Pass';
+    }else if (score>=40){
+        result='D-Ordinary Pass';
+    }else if (score>=0){
+        result='F-Fail';
+    }
+    document.getElementById('result').innerHTML=result;
 }
+
+// Validation to see if the input is a number or text
